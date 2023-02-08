efcb8529abe00524129787d3e26130e84aa332d9
udityaprakash <udityaprakash983@gmail.com>
2023-02-08 22:47:01 +0530

    ejs templating engine added to project

--- server.js
+++ server.js
@@ -2,20 +2,30 @@ const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 const app = express();
 require("dotenv").config();
+const bodyparser = require("body-parser");
 
 const configuration = new Configuration({
     apiKey: process.env.APIKEY,
 });
 const openai = new OpenAIApi(configuration);  
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine","ejs");
     
 app.get("/",async (req,res)=>{
+    res.render("body.ejs",{imgurl:''});
+    //   res.json({url:image_url});
+});
+
+app.post("/", async (req, res)=>{
     const response = await openai.createImage({
         prompt: "horse making a phonecall with its childrens surrounded in winter season outside horse yard",
         n: 5,
         size: "512x512",
       });
       image_url = response.data.data;
-      res.json({url:image_url});
+
 });
 
 
