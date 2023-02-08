5dad386101769b55578f9193eca31ebb85dfb6ac
udityaprakash <udityaprakash983@gmail.com>
2023-02-09 01:08:39 +0530

    image generation project completed with osm ui

--- server.js
+++ server.js
@@ -14,17 +14,29 @@ app.use(express.static("public"));
 app.set("view engine","ejs");
     
 app.get("/",async (req,res)=>{
-    res.render("body.ejs",{imgurl:''});
-    //   res.json({url:image_url});
+    res.render("body.ejs",{imgurl:null , errmsg:''});
 });
 
 app.post("/", async (req, res)=>{
-    const response = await openai.createImage({
-        prompt: "horse making a phonecall with its childrens surrounded in winter season outside horse yard",
-        n: 5,
-        size: "512x512",
-      });
-      image_url = response.data.data;
+    let { context } = req.body;
+    if(context){
+        if(context.length >= 15){
+
+            const response = await openai.createImage({
+                prompt: context,
+                n: 1,
+                size: "512x512",
+              });
+              image_url = response.data.data[0].url;
+              res.render("body.ejs",{imgurl: image_url , errmsg:""});
+        }else{
+            res.render("body.ejs",{imgurl:null, errmsg:"Write Something more."});
+        }
+        
+    }else{
+        res.render("body.ejs",{imgurl:null, errmsg:"Please write something to search"});
+    }
+
 
 });
 
